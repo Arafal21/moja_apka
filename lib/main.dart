7d076bd06ae893f634e451f8a50c0d9aeaa642fc
Rafal Andrzejkiewicz <rafalandrzejkiewicz@Rafals-MacBook-Air.local>
2025-12-16 22:23:57 +0100

    add inputs for form - email and password with eye

--- lib/main.dart
+++ lib/main.dart
@@ -111,35 +111,58 @@ class LoginScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          SizedBox(height: 30),
-
-          IconButton(
-            onPressed: () {
-              Navigator.pop(context);
-            },
-            icon: Icon(Icons.arrow_back),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(24.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              IconButton(
+                onPressed: () => Navigator.pop(context),
+                icon: Icon(Icons.arrow_back),
+              ),
+              SizedBox(height: 80),
+              Text(
+                "Let's Sign you in.",
+                style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 16),
+              Text(
+                'Welcome back',
+                style: TextStyle(fontSize: 24, color: Colors.grey),
+              ),
+              SizedBox(height: 8),
+              Text(
+                "You've been missed!",
+                style: TextStyle(fontSize: 24, color: Colors.grey),
+              ),
+              SizedBox(height: 40),
+              Text('Username or Email'),
+              SizedBox(height: 8),
+              TextField(
+                decoration: InputDecoration(
+                  hintText: 'Enter Username or Email',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+              Text('Password'),
+              SizedBox(height: 8),
+              TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  hintText: 'Enter Password',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  suffixIcon: Icon(Icons.visibility_off),
+                ),
+              ),
+            ],
           ),
-
-          SizedBox(height: 30),
-
-          Text(
-            "Let's Sign you in.",
-            style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
-          ),
-          SizedBox(height: 16),
-          Text(
-            'Welcome back',
-            style: TextStyle(fontSize: 24, color: Colors.grey),
-          ),
-          SizedBox(height: 8),
-          Text(
-            "You've been missed!",
-            style: TextStyle(fontSize: 24, color: Colors.grey),
-          ),
-        ],
+        ),
       ),
     );
   }
