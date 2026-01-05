2c86a11f54b094bdda246f06013e63d12fc5d987
Rafal Andrzejkiewicz <rafalandrzejkiewicz@Rafals-MacBook-Air.local>
2026-01-05 23:48:05 +0100

    add email and password inputs

--- lib/main.dart
+++ lib/main.dart
@@ -11,95 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: WelcomeScreen(),
-    );
-  }
-}
-
-class WelcomeScreen extends StatelessWidget {
-  const WelcomeScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(24.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Image.asset('assets/images/people.jpg', height: 220),
-
-              // SizedBox = padding
-              const SizedBox(height: 24),
-
-              Text(
-                'Welcome to SeekJob',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 29,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.black,
-                ),
-              ),
-
-              const SizedBox(height: 12),
-
-              Text(
-                'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-                textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 14, color: Colors.grey.shade700),
-              ),
-
-              const SizedBox(height: 24),
-
-              SizedBox(
-                width: double.infinity,
-                height: 50,
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.black,
-                    foregroundColor: Colors.white,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(12),
-                    ),
-                    elevation: 0,
-                  ),
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => const LoginScreen(),
-                      ),
-                    );
-                  },
-                  child: const Text('Login'),
-                ),
-              ),
-
-              const SizedBox(height: 14),
-
-              SizedBox(
-                width: double.infinity,
-                height: 50,
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.grey,
-                    foregroundColor: Colors.black,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(12),
-                    ),
-                    elevation: 0,
-                  ),
-                  onPressed: () {},
-                  child: const Text('Register'),
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
+      home: LoginScreen(),
     );
   }
 }
@@ -114,160 +26,96 @@ class LoginScreen extends StatelessWidget {
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.all(24.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              IconButton(
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-                icon: Icon(Icons.arrow_back),
-              ),
-              
-              SizedBox(height: 40),
-              
-              Text(
-                "Let's Sign you in.",
-                style: TextStyle(
-                  fontSize: 32,
-                  fontWeight: FontWeight.bold,
-                  fontFamily: 'Roboto',
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(Icons.arrow_back),
                 ),
-              ),
-              SizedBox(height: 16),
-              Text(
-                'Welcome back',
-                style: TextStyle(
-                  fontSize: 24,
-                  color: Colors.grey,
-                  fontFamily: 'Roboto',
-                ),
-              ),
-              SizedBox(height: 8),
-              Text(
-                "You've been missed!",
-                style: TextStyle(
-                  fontSize: 24,
-                  color: Colors.grey,
-                  fontFamily: 'Roboto',
-                ),
-              ),
-              
-              SizedBox(height: 40),
-              
-              Text('Username or Email'),
-              SizedBox(height: 8),
-              TextField(
-                decoration: InputDecoration(
-                  hintText: 'Enter Username or Email',
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(12),
+
+                SizedBox(height: 40),
+
+                Text(
+                  "Let's Sign you in.",
+                  style: TextStyle(
+                    fontSize: 32,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Roboto',
                   ),
                 ),
-              ),
-              
-              SizedBox(height: 20),
-              
-              Text('Password'),
-              SizedBox(height: 8),
-              TextField(
-                obscureText: true,
-                decoration: InputDecoration(
-                  hintText: 'Enter Password',
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(12),
+                SizedBox(height: 16),
+                Text(
+                  'Welcome back',
+                  style: TextStyle(
+                    fontSize: 24,
+                    color: Colors.grey,
+                    fontFamily: 'Roboto',
                   ),
-                  suffixIcon: Icon(Icons.visibility_off),
                 ),
-              ),
-              
-              SizedBox(height: 40),
-              
-              Row(
-                children: [
-                  Expanded(child: Divider()),
-                  Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 16),
-                    child: Text('or', style: TextStyle(color: Colors.grey)),
+                SizedBox(height: 8),
+                Text(
+                  "You've been missed!",
+                  style: TextStyle(
+                    fontSize: 24,
+                    color: Colors.grey,
+                    fontFamily: 'Roboto',
                   ),
-                  Expanded(child: Divider()),
-                ],
-              ),
-              
-              SizedBox(height: 30),
-              
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    padding: EdgeInsets.all(12),
-                    decoration: BoxDecoration(
-                      border: Border.all(color: Colors.grey),
+                ),
+
+                SizedBox(height: 40),
+
+                Text('Username or Email'),
+                SizedBox(height: 8),
+                TextField(
+                  keyboardType: TextInputType.emailAddress,
+                  autocorrect: false,
+                  decoration: InputDecoration(
+                    hintText: 'Enter Username or Email',
+                    border: OutlineInputBorder(
                       borderRadius: BorderRadius.circular(12),
                     ),
-                    child: Text('Google'),
                   ),
-                  SizedBox(width: 20),
-                  Container(
-                    padding: EdgeInsets.all(12),
-                    decoration: BoxDecoration(
-                      border: Border.all(color: Colors.grey),
+                ),
+
+                SizedBox(height: 20),
+
+                Text('Password'),
+                SizedBox(height: 8),
+                TextField(
+                  obscureText: true,
+                  enableSuggestions: false,
+                  autocorrect: false,
+                  keyboardType: TextInputType.visiblePassword,
+                  decoration: InputDecoration(
+                    hintText: 'Enter Password',
+                    border: OutlineInputBorder(
                       borderRadius: BorderRadius.circular(12),
                     ),
-                    child: Text('LinkedIn'),
+                    suffixIcon: Icon(Icons.visibility_off),
                   ),
-                  SizedBox(width: 20),
-                  Container(
-                    padding: EdgeInsets.all(12),
-                    decoration: BoxDecoration(
-                      border: Border.all(color: Colors.grey),
-                      borderRadius: BorderRadius.circular(12),
-                    ),
-                    child: Text('Facebook'),
-                  ),
-                ],
-              ),
-              
-              SizedBox(height: 80),
-              
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    "Don't have an account ? ",
-                    style: TextStyle(color: Colors.grey),
-                  ),
-                  GestureDetector(
-                    onTap: () {},
-                    child: Text(
-                      'Register',
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontWeight: FontWeight.bold,
+                ),
+
+                SizedBox(height: 40),
+
+                SizedBox(
+                  width: double.infinity,
+                  height: 50,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.black,
+                      foregroundColor: Colors.white,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12),
                       ),
                     ),
+                    onPressed: () {},
+                    child: Text('Login'),
                   ),
-                ],
-              ),
-              
-              SizedBox(height: 20),
-              
-              SizedBox(
-                width: double.infinity,
-                height: 50,
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.black,
-                    foregroundColor: Colors.white,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(12),
-                    ),
-                  ),
-                  onPressed: () {},
-                  child: Text('Login'),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
